a872378017c71ec2b11f8fe3fe52504fa3f972d6
Argenis J. Cunha <argenisjc@hotmail.com>
2025-03-05 14:07:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidEx2
+{
+    internal class Program
+    {
+        public double amountToConvert = 0;
+        const double UtoE = 0.95;
+        const double UtoJ = 25;
+        const double EtoJ = 26;
+
+        static void USDtoEUR(double amountToConvert)
+        {
+            double convertedAmount = amountToConvert * UtoE;
+            Console.WriteLine($"The Converted amount is: {convertedAmount}");
+        }
+        static void USDtoJPY(double amountToConvert)
+        {
+            double convertedAmount = amountToConvert * UtoJ;
+            Console.WriteLine($"The Converted amount is: {convertedAmount}");
+        }
+        static void EURtoJPY(double amountToConvert)
+        {
+            double convertedAmount = amountToConvert * EtoJ;
+            Console.WriteLine($"The Converted amount is: {convertedAmount}");
+        }
+
+        static void Main(string[] args)
+        {
+            double amountToConvert = 0;
+            string fromCurrency = "";
+            string toCurrency = "";
+
+            Console.WriteLine("** WELCOME TO THE CURRENCY EXCHANGE APP **");
+            Console.Write("Please enter the amount to convert: ");
+            amountToConvert = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please select the source currency:\n1. USD\n2. EUR\n3. JPY");
+            Console.Write("Make a choice: ");
+            fromCurrency = Console.ReadLine();
+
+            Console.WriteLine("Please select the target currency:\n1. USD\n2. EUR\n3. JPY");
+            Console.Write("Make a choice: ");
+            toCurrency = Console.ReadLine();
+
+            if (fromCurrency == "1" && toCurrency == "2")
+            {
+                USDtoEUR(amountToConvert);
+            }
+            else if (fromCurrency == "1" && toCurrency == "3")
+            {
+                USDtoJPY(amountToConvert);
+            }
+            else if (fromCurrency == "2" && toCurrency == "3")
+            {
+                EURtoJPY(amountToConvert);
+            }
+            else
+            {
+                Console.WriteLine("Invalid currency selection.");
+            }
+        }
+    }
+}
